bc127d5dfb126803fa33e6e988705a6581f48566
carlos500 <carloshack01@gmail.com>
2016-05-05 13:06:33 -0500

    Upload

---
+++ Tarea.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for numero in 0...100
+{
+    var division : Double = Double (numero) / 5
+    var residuo5  = numero % 5
+    var residuoP  = numero % 2
+    if residuo5 == 0
+    {
+        print("# \(numero) Bingo")
+    }
+    if residuoP == 0
+    {
+        print("# \(numero) Par")
+    }
+    else
+    {
+        print("# \(numero) Impar")
+    }
+    if numero >= 30 && numero <= 40
+    {
+        print("# \(numero) Viva Swift!!!")
+    }
+}
